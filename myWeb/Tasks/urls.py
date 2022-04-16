ab2a7ab12d8679718d909a6a318e85733ca37de5
mrReedax <andresfigueredo560@gmail.com>
2022-04-16 16:09:45 -0500

    registration url modified

--- myWeb/Tasks/urls.py
+++ myWeb/Tasks/urls.py
@@ -5,7 +5,7 @@ from tasks import views
 app_name = 'tasks'
 urlpatterns = [
     path('', views.index, name='index'),
-    path('registration/', views.registration, name='registration'),
+    path('registration', views.registration, name='registration'),
     path('home', views.home, name='home'),
     path('home/modifyTask', views.modifyTask, name='modifyTask'),
     path('records', views.records, name='records'),
